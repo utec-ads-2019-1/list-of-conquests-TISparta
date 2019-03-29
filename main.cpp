a3ca9c9af56e10e354ede3b9622a5072fe471c66
TISparta <jose.garcia@utec.edu.pe>
2019-03-28 20:52:12 -0500

    Solucion

--- main.cpp
+++ main.cpp
@@ -1,7 +1,20 @@
-#include <iostream>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-int main(int argc, char *argv[]) {
-    return 0;
+int main () {
+  int n;
+  cin >> n;
+  map <string, int> frec;
+  cin.ignore();
+  for (int i = 0; i < n; i++) {
+    string line;
+    getline(cin, line);
+    stringstream token(line);
+    string country;
+    token >> country;
+    frec[country]++;
+  }
+  for (auto pp: frec) cout << pp.first << ' ' << pp.second << '\n';
+  return (0);
 }
